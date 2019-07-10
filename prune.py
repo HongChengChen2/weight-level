bd3d054f04957e79678bd44ab9f7833b5893b478
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-07-10 12:06:35 +0800

    data

--- prune.py
+++ prune.py
@@ -123,6 +123,8 @@ def main():
             model = torch.nn.DataParallel(model).cuda()
 
     valdir = os.path.join(args.data, '')
+    print('val directory:',valdir)
+
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
