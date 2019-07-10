0d7ce31b3ee4749719f3221f9b9e1363beb1cbc9
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-07-10 11:57:05 +0800

    data

--- prune.py
+++ prune.py
@@ -122,7 +122,7 @@ def main():
         else:
             model = torch.nn.DataParallel(model).cuda()
 
-    valdir = os.path.join(args.data, 'val')
+    valdir = os.path.join(args.data, '')
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
 
