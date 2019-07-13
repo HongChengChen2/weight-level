03177c32fc546b6498f520f9806af4a32403e1de
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-07-13 16:59:06 +0800

    bug

--- trans_prune.py
+++ trans_prune.py
@@ -264,10 +264,9 @@ def validate(val_loader, model, criterion):
             loss = criterion(output, target)
 
             # measure accuracy and record loss
-            prec1, prec5 = accuracy(output, target, topk=(1, 5))
+            prec1, prec5 = accuracy(output, target, topk=(1, 1))
             losses.update(loss.item(), input.size(0))
             top1.update(prec1[0], input.size(0))
-            top5.update(prec5[0], input.size(0))
 
             # measure elapsed time
             batch_time.update(time.time() - end)
@@ -277,13 +276,11 @@ def validate(val_loader, model, criterion):
                 print('Test: [{0}/{1}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                       'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'.format(
                        i, len(val_loader), batch_time=batch_time, loss=losses,
-                       top1=top1, top5=top5))
+                       top1=top1))
 
-        print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
-              .format(top1=top1, top5=top5))
+        print(' * Prec@1 {top1.avg:.3f}'.format(top1=top1))
 
     return top1.avg
 
