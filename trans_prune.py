52e6bf128cf8dba4b1d51a2b88419622e734d439
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-07-19 15:46:17 +0800

    bug

--- trans_prune.py
+++ trans_prune.py
@@ -134,7 +134,7 @@ def main():
 
 
     valdir_train = os.path.join(args.data, 'train/')
-    valdir_test = os.path.join(args.data, 'test/')
+    valdir_test = os.path.join(args.data, 'val/')
 
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
