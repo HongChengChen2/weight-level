ba7a956185456570543da265a57e1d56bc69cc72
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-08-29 11:28:41 +0800

    bug

--- trans_prune_0.py
+++ trans_prune_0.py
@@ -290,7 +290,7 @@ def accuracy(output, target, topk=(1,)):
         #print("pred after:",pred)
 
         pred = pred.t() # a zhuanzhi transpose xcol 5row
-        #print("pred.t():",pred)
+        print("pred.t():",pred)
         #print("size:",pred[0][0].type()) #5,12
 
 
