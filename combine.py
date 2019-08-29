a1bee0afc245aa9c2f1778c1d3e681e98912c984
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-08-29 20:32:03 +0800

    bug

--- combine.py
+++ combine.py
@@ -221,7 +221,6 @@ def validate(val_loader, model_1, model_2, model_3, criterion):
             output_1= F.softmax(output_1, dim=1) # calculate as row
             print("size: ", output_1.size()) #torch.float32
             out_size = output_1.size()
-            print("type : ", out_size.dtype ) #torch.float32
             print("get one : ", out_size[0].item() ) #torch.float32
 
 
