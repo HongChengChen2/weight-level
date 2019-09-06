9adf4b3dd1341ffd382c15040614d64df91adf6d
洪成晨 <hongchengchen@hongchengchendeMacBook-Pro.local>
2019-09-06 16:19:15 +0800

    bug

--- combine_5.py
+++ combine_5.py
@@ -218,10 +218,11 @@ def validate(val_loader, model_1, model_2, model_3, criterion):
             if args.gpu is not None:
                 input = input.cuda(args.gpu, non_blocking=True)
             target = target.cuda(args.gpu, non_blocking=True) # 0*100 + 1*100 +2*100
-            print("target:",target)
+            #print("target:",target)
             # compute output,out put is a tensor
             output_1 = model_1(input)
             output_1= F.softmax(output_1, dim=1) # calculate as row
+            print(output_1)
             output_2 = model_2(input)
             output_2= F.softmax(output_2, dim=1)
             output_3 = model_3(input)
@@ -235,6 +236,9 @@ def validate(val_loader, model_1, model_2, model_3, criterion):
 
             o1_1 , o1_2 ,o1_3= output_1.chunk(3,dim=1)
             output_1 = torch.cat([o1_1,o1_2,zero_tensor,o1_3],dim=1)
+            _, output_1_max = output.topk(1, 1, True, True)
+            print(output_1_max)
+            print(bug)
 
             o2_1 , o2_2, o2_3 = output_2.chunk(3,dim=1)
             output_2 = torch.cat([o2_1,zero_tensor,o2_3,o2_2],dim=1)
@@ -243,10 +247,10 @@ def validate(val_loader, model_1, model_2, model_3, criterion):
             output_3 = torch.cat([zero_tensor, o3_2,o3_3,o3_1],dim=1)
 
             output = output_1 + output_2 + output_3
-            print("output_1:",output_1)
-            print("output_2:",output_2)
-            print("output_3:",output_3)
-            print("output:",output)
+            #print("output_1:",output_1)
+            #print("output_2:",output_2)
+            #print("output_3:",output_3)
+            #print("output:",output)
 
             loss = criterion(output, target)
 
